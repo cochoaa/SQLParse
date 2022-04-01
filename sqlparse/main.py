6f54619cc9201fb45babc3b0e718510858f16689
Carlos Ochoa <carlosoaly@gmail.com>
2022-03-31 23:42:59 -0500

    Configuracion para el contenedor

--- sqlparse/main.py
+++
@@ -1,5 +0,0 @@
-import sqlparse
-querys=sqlparse.parse("DELETE FROM bytsscom_bytsig.registro_hoja_ruta_det WHERE id_registro_hr IN (SELECT id_registro_hr FROM bytsscom_bytsig.registro_hoja_ruta WHERE id_hoja_ruta_actual=31081) AND id_corr>4;UPDATE bytsscom_bytsig.registro_hoja_ruta set  id_hoja_ruta_actual=29732, id_corr_actual=4 where id_registro_hr in (SELECT id_registro_hr FROM bytsscom_bytsig.registro_hoja_ruta WHERE id_hoja_ruta_actual=31081);")
-for query in querys:
-	for token in query.tokens:
-		print(token.value + ' ' + str(token.ttype))
