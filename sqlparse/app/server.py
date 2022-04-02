32c45cfb8a81d9d08adfd6000a3f8df297d1b70e
Carlos Ochoa <carlosoaly@gmail.com>
2022-04-01 21:28:27 -0500

    Config para vista

--- sqlparse/app/server.py
+++ sqlparse/app/server.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template,send_from_directory
+import os
 app = Flask(__name__)
+# app.add_url_rule('/favicon.ico',
+#                  redirect_to=url_for('static', filename='img/favicon.png'))
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'img/favicon.png', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
-def hello_world():
-    return "Usage: http://<hostname>[:<prt>]/api/<url>"
+def index():
+    data = {
+        'titulo': 'Convesor de Sentencias SQL'
+    }
+    return render_template('index.html', data=data)
 
+@app.route('/converter')
+def conveter():
+    data = {
+        'titulo': 'Converter',
+        'querys_input': '',
+        'querys_output': ''
+    }
+    return render_template('converter_statament.html', data=data)
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000)
-
-@app.route("/api/<path:parameter>")
-def api(parameter):
-    return 'Parametro: '+parameter
+    app.run(host="0.0.0.0",port=8000,debug=True)
