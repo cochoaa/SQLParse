0cff56e29f1ce169900398c9cbc2c037b4ad208d
Carlos Ochoa <carlosoaly@gmail.com>
2022-04-11 20:16:42 -0500

    Correccion para deliminar keyword SET y sentencia sin WHERE

--- sqlparse/app/statamentService.py
+++ sqlparse/app/statamentService.py
@@ -1,5 +1,5 @@
 import sqlparse
-from sqlparse.sql import Statement, Token
+from sqlparse.sql import Statement, Token, Where
 from sqlparse import tokens as TokenType
 
 token_select = Token(TokenType.Keyword.DML,'SELECT')
@@ -7,17 +7,21 @@ token_space = Token(TokenType.Text.Whitespace,' ')
 token_asterist = Token(TokenType.Wildcard,'*')
 token_from = Token(TokenType.Keyword, 'FROM')
 
-def remove_all_extra_spaces(string:str):
-    return " ".join(string.split())
-
 def format_statament(string:str):
-    string_statament = sqlparse.format(string, strip_comments=True, keyword_case='upper')
-    print(string_statament)
+    """
+    Remove WhiteSpace after sentence
+    :param string:
+    :return:
+    """
+    string_statament = sqlparse.format(string, strip_comments=True)
     return string_statament
+
+def remove_all_extra_spaces(string:str):
+    string=" ".join(string.split())
+    return format_statament(string)
 
 def select_converter(string_stataments: str):
     string_stataments=remove_all_extra_spaces(string_stataments)
-    string_stataments=format_statament(string_stataments)
     tuple_statament = sqlparse.parse(string_stataments)
     print("Cantidad de querys: " + str(len(tuple_statament)))
     validate_stataments(tuple_statament)
@@ -28,9 +32,8 @@ def select_converter(string_stataments: str):
         if statament.get_type()!='INSERT' and statament.get_type()!='SELECT':
             string_select = str(get_statament_converted(statament))
         list_select.append(string_select)
-        print('Statement Final: ' + str(string_select))
+        print('Statement   Final: ' + str(string_select))
     return list_select;
-
 
 def validate_stataments(tuple_statament):
     for statament in tuple_statament:
@@ -39,80 +42,54 @@ def validate_stataments(tuple_statament):
         # if statament.get_type() == 'SELECT':
         #     raise Exception("Sentencia SELECT no es permitida :" + str(statament))
 
+def get_stat_select_from():
+    stat_select_from=[token_select,token_space,token_asterist,token_space,token_from]
+    return stat_select_from;
+
+def replace_dml_for_select(statament: Statement):
+    list_tokens = statament.tokens
+    stat_select_from=get_stat_select_from()
+    del list_tokens[0]
+    list_tokens[0:0]=stat_select_from
+
+def is_token_set(token:Token):
+    return token.value.upper()=='SET' and token.ttype==TokenType.Keyword
+
+def is_token_final(token:Token):
+    return token.value.upper()==';' and token.ttype==TokenType.Punctuation
+
+def is_token_where(token:Token):
+    return isinstance(token, Where)
+
+def remove_keyword_set(statament: Statement):
+    list_tokens = statament.tokens
+    index_token_set=index_token_where=index_token_final=0
+    for i, token in enumerate(list_tokens):
+        if is_token_set(token):
+            index_token_set = i
+        if is_token_where(token):
+            index_token_where = i
+        if is_token_final(token):
+            index_token_final = i
+    if index_token_set<index_token_where:
+        del list_tokens[index_token_set:index_token_where]
+    else:
+        del list_tokens[index_token_set:index_token_final]
+
+
 def get_statament_converted(statament: Statement):
-    list_tokens=statament.tokens
-    if statament.get_type() == 'UPDATE':
-        list_tokens.pop(0)
-        list_tokens.insert(0,token_select)
-        list_tokens.insert(1, token_space)
-        list_tokens.insert(2, token_asterist)
-        list_tokens.insert(3, token_space)
-        list_tokens.insert(4, token_from)
-        list_tokens.pop(8)#set
-        list_tokens.pop(8)#space
-        list_tokens.pop(8)#valores
-    elif statament.get_type() == 'DELETE':
-        list_tokens.pop(0)
-        list_tokens.insert(0,token_select)
-        list_tokens.insert(1,token_space)
-        list_tokens.insert(2,token_asterist)
+    DML_TYPE=statament.get_type()
+    if DML_TYPE in {'UPDATE', 'DELETE'}:
+        replace_dml_for_select(statament)
+        if DML_TYPE in {'UPDATE'}:
+            remove_keyword_set(statament)
     return statament;
 
-
-
 if __name__ == "__main__":
-    print("File1 is being run directly")
-    strings_quey = '''              
-
-
-SELECT * FROM bytsscom_bytsig.certificacion_meta WHERE id_certificacion=249594;
-
-UPDATE bytsscom_bytsig.certificacion_meta 
-   SET mont_meta_cert=23067.00 , mont_meta_nac=23067.00
- WHERE id_certificacion=249594;
-
-
-SELECT * FROM bytsscom_bytsig.registro_meta WHERE id_registro=147493;
-
-UPDATE bytsscom_bytsig.registro_meta 
-SET monto_meta=23067.00, monto_soles=23067.00
-WHERE id_registro=147493;
-
-SELECT * FROM bytsscom_bytsig.certificacion WHERE id_certificacion=249594;
-
-UPDATE bytsscom_bytsig.certificacion 
-   SET esta_cert='A'
- WHERE id_certificacion=249594;
-
-
-SELECT * FROM bytsscom_bytsig.certificacion_sec WHERE id_certificacion=249594;
-
-UPDATE bytsscom_bytsig.certificacion_sec 
-   SET estado_sec='A'
- WHERE id_certificacion=249594;
-
-
-
-SELECT * FROM bytsscom_bytsig.certificacion WHERE id_certificacion=250131;
-SELECT * FROM bytsscom_bytsig.certificacion_meta WHERE id_certificacion=250131;
-SELECT * FROM bytsscom_bytsig.certificacion_sec WHERE id_certificacion=250131;
-
-
-INSERT INTO      bytsscom_bytsig.certificacion(id_certificacion, id_anio,       fech_cert, esta_cert, siaf_certificado , tipo_cert, id_certificacion_sup, id_certdoc, id_referencia,  siaf_cert_secuencia,                                                                                                            desc_doc,            sys_fech_registro, id_tipooperacion, id_fuente, siaf_tipo_finan, num_transferencia, id_memo_requerimiento, id_proyecto, id_tipo_doc_ccp) 
-				        VALUES(          250131,    2022,    '2022-03-29',       'A',      '0000004317',      'CA',               249289,      'AME',        287967,               '0003', '22-0002555 ENCARGO DEL PROYECTO: Contrato N°  28-2018-FONDECYT-BM-IADT-AV   A FAVOR DE :  ARAKAKI MAKISHI, MONICA', '2022-03-28 21:15:13.132828',              'A',        15,             'T',           '56578',                287967,        3598,         'PRENC');
-
-
-INSERT INTO bytsscom_bytsig.certificacion_meta(id_certificacion, id_certificacion_sec, id_meta_corr, id_item, id_tarea_meta, mont_meta_cert, id_clasificador, mont_meta_nac)
-				        VALUES(          250131,                    1,            1,       0,          9247,        7968.15,            1546,       7968.15);
-
-
-INSERT INTO  bytsscom_bytsig.certificacion_sec(id_certificacion, id_certificacion_sec, estado_sec,   id_moneda,   tipo_camb_sec,                 nro_doc, siaf_correlativo,      fech_doc, id_tipo_documento)
-				        VALUES(          250131,                    1,        'A',         390,        1.000000,       'RD-192-DGA-2022',           '0001',  '2022-03-28',               234);	
-
-
-'''
-
-    list_querys_select=select_converter(strings_quey)
+    strings_query = '''
+    
+    '''
+    list_querys_select=select_converter(strings_query)
     print('/*---------Select generado por Conveter SQL---------------------------------------*/')
     for query in list_querys_select:
         print(query)
