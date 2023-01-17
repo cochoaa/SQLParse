91f7583082753c6891d7d53c12760b319e66e169
Carlos Ochoa <carlosoaly@gmail.com>
2023-01-17 08:36:03 -0500

    Configurado ecpg de postgres para revisar sintaxis

---
+++ sqlparse/app/checkSintax.py
@@ -0,0 +1,18 @@
+from pgsanity.pgsanity import check_string
+
+# return 0,si es exitoso
+def check_statament(query:str):
+    success, msg = check_string(query, add_semicolon=True)
+    message=''
+    if not success:
+        # possibly show the filename with the error message
+        message =  msg  + ' --> ' + query
+    return success,message
+
+if __name__ == "__main__":
+    strings_query = '''UPDATE bytsscom_bytsig.contrato_progdes SET mont_progdes = 38449.75 WHERE id_contrato= 127982 WHERE id_progdesembolso = 16053; 
+    '''
+    success, msg =check_statament(strings_query)
+    result = 0
+    print(msg)
+
